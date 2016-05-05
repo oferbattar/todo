bb98b1529536c64848976f791a37aa025993b37e
Ofer Ben Attar <oferbattar@gmail.com>
2016-05-05 09:32:12 +0300

    DELETE  added

--- server.js
+++ server.js
@@ -45,7 +45,17 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
-
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id : todoId});
+	
+	if (matchedTodo) {
+		todos=_.without(todos,matchedTodo);
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
