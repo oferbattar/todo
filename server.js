a706543be80c5c04bcedb639082f954687150595
Ofer Ben Attar <oferbattar@gmail.com>
2016-05-15 16:45:43 +0300

    update delete and push

--- server.js
+++ server.js
@@ -67,47 +67,57 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where:{
+			id: todoId
+			}
+	}).then(function(rowsDeleted){
+		if (rowsDeleted===0) {
+			res.status(404).json({
+				eror: "no todo with id"
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
+	
 	});
-
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	
 });
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
+	
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
+	if (body.hasOwnProperty('completed')  ) {
+		attributes.completed = body.completed;
+	} 
+	
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	} 
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+	db.todo.findById(todoId).then(function(todo){
+		if (todo){
+			 todo.update(attributes).then(function(todo){
+		res.json(todo.toJSON());
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	}, function(e){
+
+	res.status(400).json(e);	
+	});
+		} else{
+		res.status(404).send();
+		}
+	}, function(){
+		res.status(500).send();
+	});
+	
 });
 
 db.sequelize.sync().then(function() {
