e32cea4ef6c3bfb491ab5965ed5b9f1c3570fe15
Ofer Ben Attar <oferbattar@gmail.com>
2016-07-27 15:54:04 +0300

    token exp

--- db.js
+++ db.js
@@ -16,11 +16,11 @@ if (env === 'production') {
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
-//db.user = sequelize.import(__dirname + '/models/user.js' );
+db.user = sequelize.import(__dirname + '/models/user.js' );
 db.token = sequelize.import(__dirname + '/models/token.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-//db.Sequelize = Sequelize;
+db.Sequelize = Sequelize;
 db.todo.belongsTo(db.user);
 db.user.hasMany(db.todo);
 module.exports = db;
