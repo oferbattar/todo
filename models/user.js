d9881b3ef8d7ddbe79db2d9b3a8d2bf10bb964f1
Ofer Ben Attar <oferbattar@gmail.com>
2016-07-27 15:53:10 +0300

    token exp

--- models/user.js
+++ models/user.js
@@ -65,6 +65,26 @@ module.exports = function(sequelize, DataTypes) {
 						reject();
 					});
 				});
+			},
+			findByToken: function(token){
+				return new Promise( function(resolve , reject){
+						try{
+							var decodedJMT = jwt.verify(token,'qwerty098' );
+							var bytes = cryptojs.AES.decrypt(decodedJMT.token, 'abc123!@#!');
+							var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+							user.findById(tokenData.id).then(function(user){
+								if (user){
+									resolve(user);
+								}else{
+									reject();
+								}
+							}, function(e){
+								reject();
+							});
+						} catch(e){
+							reject();
+						}
+				});
 			}
 		},
 		instanceMethods: {
@@ -82,7 +102,7 @@ module.exports = function(sequelize, DataTypes) {
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
 					var token = jwt.sign({
 						token: encryptedData
-					}, 'qwerty098');
+						}, 'qwerty098');
 
 					return token;
 				} catch (e) {
