f6844e4a2cd3b667d72c80e96de91e8b10b11438
Ofer Ben Attar <oferbattar@gmail.com>
2016-07-31 11:22:07 +0300

    9.10

--- server.js
+++ server.js
@@ -46,7 +46,12 @@ app.get('/todos', middleware.requireAuthentication ,function(req, res) {
 app.get('/todos/:id',  middleware.requireAuthentication ,function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -92,7 +97,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -120,8 +126,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	if (body.hasOwnProperty('description')) {
 		attributes.description = body.description;
 	}
-
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -133,6 +143,7 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		}
 	}, function() {
 		res.status(500).send();
+	
 	});
 });
 
